cb5a05703a69f75861e3a8cfbe19edf5fc0e498a
Aye Cap <capcap120@gmail.com>
2020-09-22 03:24:21 +0900

    Homepage with only outlines & thumbs and no animations

--- zero-client/src/components/Homepage/Homepage.tsx
+++ zero-client/src/components/Homepage/Homepage.tsx
@@ -1,25 +1,52 @@
 import React, { Component } from "react";
-import { Carousel } from "antd";
 import "./Homepage.scss";
 
 export class Homepage extends Component {
   public render() {
     return (
       <div className="homepage">
-        <Carousel autoplay={true}>
-          <div>
-            <h3 className="content">1</h3>
+        {/* Grid Outlines */}
+        <div className="grid">
+          <div className="grid-box"></div>
+          <div className="grid-box"></div>
+          <div className="grid-box"></div>
+          <div className="grid-box"></div>
+          <div className="grid-box"></div>
+        </div>
+        {/* Grid Thumbs */}
+        <div className="grid">
+          <div className="grid-content">
+            <img
+              className="grid-1"
+              src="https://images.unsplash.com/photo-1548681528-6a5c45b66b42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80"
+              alt=""
+            ></img>
           </div>
-          <div>
-            <h3 className="content">2</h3>
+          <div className="grid-content">
+            <img
+              className="grid-2"
+              src="https://images.unsplash.com/photo-1517451330947-7809dead78d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80"
+              alt=""
+            ></img>
           </div>
-          <div>
-            <h3 className="content">3</h3>
+          <div className="grid-content">
+            <img
+              className="grid-3"
+              src="https://images.unsplash.com/photo-1497910091122-9f8a7746eb33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80"
+              alt=""
+            ></img>
           </div>
-          <div>
-            <h3 className="content">4</h3>
+          <div className="grid-content">
+            <img
+              className="grid-4"
+              src="https://images.unsplash.com/photo-1599727572359-53e2b1d0d012?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2706&q=80"
+              alt=""
+            ></img>
           </div>
-        </Carousel>
+          <div className="menu">
+            <span>Wip.</span>
+          </div>
+        </div>
       </div>
     );
   }
